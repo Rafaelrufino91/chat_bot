e2b231fdb9cc2035cc7dbf98cd01db13fcc0400d
Rafael da silva rufino <rafaelsilvarufino@gmail.com>
2022-08-12 17:34:46 -0300

    Atualizacao do chatbot para envio de mensagem no whatsapp

--- whatsappbot.py
+++ whatsappbot.py
@@ -1,32 +1,37 @@
 from selenium import webdriver
 from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+# Definindo o caminho que vai acessar e baixando o driver do navegador
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://web.whatsapp.com/")
+sleep(10)
+
+# Mensagem que vai se enviada e para quem vai ser enviado
+mensagem = "Testando o envio de mensagem. Nao precisa responder"
+conversas = ["Gui Gui", "Naturfit"]
 
 
-class WhatsappBot:
-    def __init__(self):
-        self.mensagem = "Testando o envio de mensagem com bot"
-        self.conversas = ["Amor", "Naturfit"]
-        options = webdriver.ChromeOptions()
-        options.add_argument("lang=pt-br")
-        self.driver = webdriver.Chrome(executable_path='/home/Rafael/Documentos/GitHub/chatbot/chromedriver')
-
-    def enviarmensagem(self):
-        self.driver.get("https://web.whatsapp.com/")
-        sleep(30)
-        for grupo in self.conversas:
-            grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
-            sleep(4)
-            grupo.click()
-            chat_box = self.driver.find_element_by_class_name("p3_M1")
-            sleep(4)
-            chat_box.click()
-            chat_box.send_keys(self.mensagem)
-            botao_enviar = self.driver.find_element_by_xpath("//span[@data-icon='send']")
-            sleep(4)
-            botao_enviar.click()
-            sleep(4)
-
-bot = WhatsappBot()
-bot.enviarmensagem()
+# Aqui ele vai buscar o contato na pesquisa
+def buscar_contato(contato):
+    pesquisar_contato = driver.find_element("xpath", '//div[contains(@class, "copyable-text selectable-text")]')
+    sleep(3)
+    pesquisar_contato.click()
+    pesquisar_contato.send_keys(contato)
+    sleep(3)
+    pesquisar_contato.send_keys(Keys.ENTER)
 
 
+def enviarmensagem(mensagem):
+    chat_box = driver.find_element("xpath", '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div['
+                                            '2]/div[1]')
+    chat_box.click()
+    sleep(3)
+    chat_box.send_keys(mensagem)
+    chat_box.send_keys(Keys.ENTER)
+
+
+for conversa in conversas:
+    buscar_contato(conversa)
+    enviarmensagem(mensagem)
